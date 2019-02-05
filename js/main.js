9b5ca3a31cdb428e376603f063296c176d5d7a7c
Naimul-Islam-Siam <hallowjokes4@gmail.com>
2019-02-06 05:14:07 +0600

    update 2.0

--- js/main.js
+++ js/main.js
@@ -1,38 +1,49 @@
 var square = document.querySelectorAll(".square");
 var turn = "X";
 var message = document.querySelector(".message");
+var gameover = false;
+var resetBtn = document.querySelector(".resetBtn");
 
 for(var i = 0; i < square.length; i++){
 	square[i].addEventListener("click",function(){
-		if(this.textContent === ""){
-			this.textContent = turn;
-			switchTurn();
+		if(!gameover){
+			if(this.textContent === ""){
+				this.textContent = turn;
+				switchTurn(this);
+			}
+			else{
+				message.textContent = "Invalid. Try an empty square."
+			}
 		}
+
 		else{
-			message.textContent = "Invalid. Try an empty square."
+			message.textContent = turn + " already won!";
 		}
 	});
 }
 
-function switchTurn(){
-	if(decision(turn)){
+function switchTurn(y){
+	if(decision(turn, y)){
+		gameover = true;
 		message.textContent = "Congrats " + turn + " you're the winner !!";
 	}
 	else if(turn === "X"){
 		turn = "O";
+		y.style.color = '#545454';
 		message.textContent = "It's " + turn + " 's turn.";
 	}
 	else{
 		turn = "X";
+		y.style.color = '#fff';
 		message.textContent = "It's " + turn + " 's turn.";
 	}
 	
 	return turn;	
 }
 
-function decision(move){
+function decision(move, y){
 	var result = false;
-	if(check(1, 2, 3, move) ||
+	if( check(1, 2, 3, move) ||
 		check(4, 5, 6, move) ||
 		check(7, 8, 9, move) ||
 		check(1, 4, 7, move) ||
@@ -42,6 +53,11 @@ function decision(move){
 		check(3, 5, 7, move)){
 			result = true;
 	}
+	if(turn === "X")
+		y.style.color = '#545454';
+	else if(turn === "O")
+		y.style.color = '#fff';
+
 	return result;
 }
 
@@ -57,4 +73,15 @@ function getValue(number){
 	return document.querySelector("#s" + number).textContent;
 }
 
+function clearBox(number){
+	document.querySelector("#s" + number).textContent = "";
+}
 
+resetBtn.addEventListener("click", function(){
+	for(var i = 1; i <= square.length; i++){
+		clearBox(i);
+	}
+	message.textContent = "Start Again";
+	gameover = false;
+	turn = "X"; 
+});
